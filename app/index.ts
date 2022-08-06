b62d13ebdcbd00b5e39ea4ba1106c1026699240a
narumincho <16481886+narumincho@users.noreply.github.com>
2022-08-06 18:34:23 +0900

    年月日の表示

--- app/index.ts
+++ app/index.ts
@@ -2,13 +2,16 @@ import clock from "clock";
 import document from "document";
 import { preferences } from "user-settings";
 
-const padSpace = (value: number): string => {
+const format2Digit = (value: number): string => {
   if (value < 10) {
     return "0" + value.toString();
   }
   return value.toString();
 };
 
+const yearElement = document.getElementById("year");
+const monthElement = document.getElementById("month");
+const dayElement = document.getElementById("day");
 const hoursElement = document.getElementById("hours");
 const minutesElement = document.getElementById("minutes");
 const secondsElement = document.getElementById("seconds");
@@ -16,7 +19,10 @@ const secondsElement = document.getElementById("seconds");
 clock.granularity = "seconds";
 clock.addEventListener("tick", (evt) => {
   const today = evt.date;
-  hoursElement.text = padSpace(today.getHours());
-  minutesElement.text = padSpace(today.getMinutes());
-  secondsElement.text = padSpace(today.getSeconds());
+  yearElement.text = today.getFullYear().toString();
+  monthElement.text = format2Digit(today.getMonth() + 1);
+  dayElement.text = format2Digit(today.getDate());
+  hoursElement.text = format2Digit(today.getHours());
+  minutesElement.text = format2Digit(today.getMinutes());
+  secondsElement.text = format2Digit(today.getSeconds());
 });
