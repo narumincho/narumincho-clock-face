6cbe79d7b004395f555d16ae57709c82e4003fbe
narumincho <16481886+narumincho@users.noreply.github.com>
2022-08-06 16:48:15 +0900

    秒数のみ表示

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+
+// Update the clock every minute
+clock.granularity = "seconds";
+
+const myLabel = document.getElementById("clock-label");
+
+clock.granularity = "seconds";
+
+clock.addEventListener("tick", (evt) => {
+  const today = evt.date;
+  myLabel.text = today.getSeconds().toString();
+});
